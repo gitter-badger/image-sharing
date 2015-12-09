7eb981dd1ac7b0f0a95ad6d0842b2d96a5118fb0
WEBER Antoine <ghostsmaker@hotmail.com>
2015-12-09 20:04:39 +0100

    reception et envoi de message

--- index.js
+++ index.js
@@ -2,24 +2,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 6000;
+var port = 7000;
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('user connexion');
+  socket.on('message', function(msg){
+    console.log('je renvoi '+msg);
+    io.emit('message', "tkt j'ai recu ->"+msg);
+  });
 });
+
+
+/*app.get('/', function(req, res){
+  res.sendfile('index.html');
+});*/
 
 http.listen(port , function(){
   console.log('listening on *:'+port );
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('je renvoi '+msg);
-    io.emit('chat message', msg);
-  });
 });
